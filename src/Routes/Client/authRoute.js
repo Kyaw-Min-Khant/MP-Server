662697c410ad13f1420c0a7a4dbcd60e4b074615
Kyaw-Min-Khant <kyawminkhant8237@gmail.com>
2023-12-17 19:21:30 +0630

    Fix_User_Route

--- src/Routes/Client/authRoute.js
+++ src/Routes/Client/authRoute.js
@@ -1,12 +1,6 @@
 import express from "express";
-import {
-  getUser,
-  login,
-  register,
-} from "../../Controllers/Client/UserController.js";
-import { verifyToken } from "../../middleware/authmiddleware.js";
+import { login, register } from "../../Controllers/Client/UserController.js";
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
-router.get("/user", verifyToken, getUser);
 export default router;
