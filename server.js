f4ffecf800e1a47c17c04f18a0a424c59b7f3c13
Kyaw-Min-Khant <kyawminkhant8237@gmail.com>
2024-02-25 23:17:24 +0630

    Cors Add

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(
   express.urlencoded({ limit: "60mb", extended: true, parameterLimit: 1000000 })
 );
